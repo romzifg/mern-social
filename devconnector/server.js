263ac50baf450151b8e1d5b33d442aa57e45b864
Romzi Farhan <romzifg@gmail.com>
2021-10-17 12:54:30 +0700

    setup db & route

--- devconnector/server.js
+++ devconnector/server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
 const app = express();
+
+app.use(express.json());
+
+// DB config
+const db = require('./config/keys').mongoURL;
+
+// Connect MongoDb
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello')
 });
+
+// Use Routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000;
 
